d94b03c4e4acb54140a71c7ceb9a0669eb08c84a
MaDMaT2106 <bodikkonovalov@gmail.com>
2023-02-19 20:19:00 +0200

    adaptive + slider

--- src/components/WeekOrDayForecast/ForecastBlockList.jsx
+++ src/components/WeekOrDayForecast/ForecastBlockList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { week } from '../TodaySidebar/TimeAndDate';
 import DayOfWeekBlock from './DayOfWeekBlock';
+import Slider from 'react-slick';
 
 export default function ForecastBlockList() {
   const forecast = useSelector((state) => state.forecast.forecast);
@@ -16,15 +17,75 @@ export default function ForecastBlockList() {
       nightTemp: day.temp.night,
     },
   }));
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 7,
+    swipe: true,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 735,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 920,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 1124,
+        settings: {
+          slidesToShow: 7,
+          slidesToScroll: 7,
+        },
+      },
+      {
+        breakpoint: 1260,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        },
+      },
+    ],
+  };
+
   return (
     <div>
-      {
+      <Slider {...settings} className="forecastList">
+        {dailyForecast?.slice(1).map((day) => {
+          return <DayOfWeekBlock dayInfo={day} key={Math.random() * 100} />;
+        })}
+      </Slider>
+
+      {/* {
         <ul className="forecastList">
           {dailyForecast?.slice(1).map((day) => {
             return <DayOfWeekBlock dayInfo={day} key={Math.random() * 100} />;
           })}
         </ul>
-      }
+      } */}
     </div>
   );
 }
